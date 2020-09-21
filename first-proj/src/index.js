b8fa0eeae35e6e8a0acbde86d8eab79daeba1431
NitishAndola <nitish.andola@gmail.com>
2020-09-21 10:43:53 +0530

    Added JSHint file to enable es6 support

--- first-proj/src/index.js
+++ first-proj/src/index.js
@@ -1,16 +1,33 @@
 //Import and Manage Libraries
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-
-
-//Create a React Component
-const App=function(){
-    return <h1>Hello World!</h1>
+function buttonText() {
+  var i = 0;
+  var msg= "Click me " + i + " times";
+   setTimeout(function () {
+    i++;
+    msg="Click me " + i + " times";
+  }, 5000);
+  return msg;
 }
-
+//Create a React Component
+const App = () => {
+  //const buttonText = "Click me!";
+  return (
+    <div>
+      <label class="label" for="name">
+        User Name
+      </label>
+      <input id="name" />
+      <button
+        style={{ color: "white", backgroundColor: "green", marginLeft: "5px" }}
+      >
+        {buttonText()}
+      </button>
+    </div>
+  );
+};
 
 //Export and show on browser
-ReactDOM.render(
-    <App/>, document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
