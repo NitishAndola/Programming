c8086a3e01ccf9b2bccc67ea062f3efae840e6f6
ANDOLA Nitish <nitish.andola@hexagon.com>
2021-01-22 07:55:44 +0530

    Class added to seasons

--- Seasons/src/index.js
+++ Seasons/src/index.js
@@ -2,13 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonsDisplay from "./SeasonsDisplay";
 
-const App = () => {
-  return (
-    <div>
-      <div>Hello World</div>
-      <SeasonsDisplay/>
-    </div>
-  );
-};
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (result) => console.log(result),
+//     (err) => console.error(err)
+//   );
+  
+//   return (
+//     <div>
+//       <div>Hello World</div>
+//       <SeasonsDisplay />
+//     </div>
+//   );
+// };
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render (){
+    window.navigator.geolocation.getCurrentPosition(
+      (result) => console.log(result),
+      (err) => console.error(err)
+    );
+
+    return (
+      <div>
+        <div>Hello World</div>
+        <SeasonsDisplay />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
